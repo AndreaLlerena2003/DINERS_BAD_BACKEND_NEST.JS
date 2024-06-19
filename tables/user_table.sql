abff6a335243c82184a682a56f1bbfc03432abbc
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-06-19 14:38:37 -0500

    update user_repository

--- tables/user_table.sql
+++ tables/user_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     type_of_document VARCHAR(50) NOT NULL, 
     number_of_document VARCHAR(255) NOT NULL,
-    last_8_digits VARCHAR(8) NOT NULL,
+    cardNumber VARCHAR(255) NOT NULL,
     expiration_date VARCHAR(50) NOT NULL, 
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL
