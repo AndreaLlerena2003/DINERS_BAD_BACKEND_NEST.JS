c4fab4d1c1b1b04636c3f7f78bb89fe9cac86728
Kamiones <carlosquinonescosta2003@gmail.com>
2024-07-07 16:11:15 -0500

    Habilitación de CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow only this origin
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE', // Allow these HTTP methods
+    allowedHeaders: 'Content-Type, Accept', // Allow these headers
+    credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
+  });
   await app.listen(3000);
 }
 bootstrap();
