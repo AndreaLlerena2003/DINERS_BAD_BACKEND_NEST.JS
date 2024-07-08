56c5a5287b58491b084886c35bc84789876c5d54
AndreaLlerena2003 <98197581+AndreaLlerena2003@users.noreply.github.com>
2024-07-07 21:09:54 -0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3001', // Allow only this origin
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE', // Allow these HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Allow these headers
+    allowedHeaders: ['authorization', 'Content-Type'], // Allow these headers
     credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
   });
   await app.listen(3000);
