7f3310bca725250d63924bc4a499246679543a00
Kamiones <carlosquinonescosta2003@gmail.com>
2024-07-10 14:15:52 -0500

    Last version

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: '*', // Allow only this origin
+    origin: 'http://localhost:5173', // Allow only this origin
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE', // Allow these HTTP methods
-    allowedHeaders: 'Content-Type, Accept', // Allow these headers
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allow these headers
     credentials: true, // Allow credentials (cookies, authorization headers, TLS client certificates)
   });
   await app.listen(3000);
